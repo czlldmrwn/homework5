d2cfb7813f1faa5c9016db12653e8f602c8189f5
полина <126289324+czlldmrwn@users.noreply.github.com>
2023-04-01 02:06:41 +0300

    Add files via upload

---
+++ homework5/src/main/java/com/kt/mirea/example/Homework5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kt.mirea.example;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author НР
+ */
+public class Homework5 {
+
+    public static void main(String[] args) {
+        System.out.println("Вариант 5, РИБО-04-21, Волженская Полина Александровна");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите путь к объекту для десереализации: ");
+        String yourPath = scan.nextLine();
+        LoaderRunnable lr = new LoaderRunnable(yourPath);
+        Thread th = new Thread(lr);
+        th.start();
+    }
+}
